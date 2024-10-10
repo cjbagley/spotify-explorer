72aaf27fd204748f7b6ddc0a952d09bf043e96e0
Colin Bagley <cjbagley@googlemail.com>
2024-10-10 12:25:12 +0100

    feat: Tidy default pages, removing 'welcome' and making controller for index

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', DashboardController::class)->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
