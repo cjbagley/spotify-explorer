441c7d5d0e19bdf8dc70f411ff768704d7b481a8
Colin Bagley <cjbagley@googlemail.com>
2024-10-12 11:29:03 +0100

    feat: Remove 'up' endpoint

--- tests/Architecture/RoutesTest.php
+++ tests/Architecture/RoutesTest.php
@@ -5,11 +5,6 @@ use Illuminate\Routing\Route;
 test('routes have names', function () {
     $route_name_missing = collect(app('router')->getRoutes())
         ->filter(function (Route $route) {
-            // Laravel 'up' route for healthcheck does not have name
-            if ($route->uri() === 'up') {
-                return false;
-            }
-
             return empty($route->getName());
         });
 
